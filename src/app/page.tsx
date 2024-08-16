690cda75925ae26d2352ae8e513199274396cb23
Sandoche <sandoche@protonmail.com>
2024-08-16 14:47:28 +0200

    ui: adding dummy content without styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,14 +34,40 @@ export default function CurrentAuction() {
 
   return (
     <main className="">
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl">Auction #{auction.round}</h1>
-        <Chip>
-          <StatusIndicatorIcon />
-          In progress
-        </Chip>
-      </div>
-      <ProgressBar progress={auction.progress} />
+      <section>
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl">Auction #{auction.round}</h1>
+          <Chip>
+            <StatusIndicatorIcon />
+            In progress
+          </Chip>
+        </div>
+        <ProgressBar progress={auction.progress} />
+        <p>
+          Closing in <span>4d 23h 33m 2s</span>
+        </p>
+        <p>
+          Ending <span>Mon 11th of August 2024 - 23:59 CET</span>
+        </p>
+      </section>
+      <section>
+        <h2>Current total auctioned value</h2>
+        <p>$1,200,100.00</p>
+        <div>TABLE WILL BE HERE</div>
+      </section>
+      <section>
+        <div>
+          <h2>Current highest bid</h2>
+          <Chip>🔥 12x cheaper than market value</Chip>
+        </div>
+        <div>
+          <span>{auction.currentHighestBidinEvmos} EVMOS</span>
+          <span>${auction.currentHighestBidInUsd}</span>
+          <a href="#">{auction.currentHighestBidder}</a>
+        </div>
+        <div>Place a bid widget will be here</div>
+        <div>Bidding History</div>
+      </section>
     </main>
   );
 }
