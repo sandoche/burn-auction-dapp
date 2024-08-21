6e617fd9136edc65ef98b1c246bcc852f04b0293
Sandoche <sandoche@protonmail.com>
2024-08-21 19:05:38 +0100

    test: adding tests with vitest

--- src/queries/__tests__/fetchCurrentAuction.spec.ts
+++ src/queries/__tests__/fetchCurrentAuction.spec.ts
@@ -3,7 +3,7 @@ import { rpcFetchCurrentAuctionInfo } from "../fetchCurrentAuction";
 import { AuctionInfo } from "@/types/AuctionInfo";
 
 describe("rpcFetchCurrentAuctionInfo()", async () => {
-  it("should return the current auction info", async () => {
+  it("should return the current auction info of type AuctionInfo", async () => {
     const result = await rpcFetchCurrentAuctionInfo();
     expect(result).toBeDefined();
     expectTypeOf(result).toMatchTypeOf<AuctionInfo>();
