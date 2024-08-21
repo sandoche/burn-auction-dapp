7fc210f42c2cd550bdb2b00f93282ac37e2fca7f
Sandoche <sandoche@protonmail.com>
2024-08-21 15:32:18 +0100

    dep: installing viem

---
+++ src/utilities/viem.ts
@@ -0,0 +1,23 @@
+import { createPublicClient, http, defineChain } from "viem";
+
+const chainId = parseInt(process.env.CHAIN_ID as string);
+const rpcUrl = process.env.RPC_URL as string;
+
+const evmos = defineChain({
+  id: chainId,
+  name: "Evmos",
+  network: "evmos",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Evmos",
+    symbol: "EVMOS",
+  },
+  rpcUrls: {
+    default: { http: [rpcUrl] },
+  },
+});
+
+const viemClient = createPublicClient({
+  chain: evmos,
+  transport: http(),
+});
