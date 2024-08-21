fef5001b37883c426729c7a81b2730c097017171
Sandoche <sandoche@protonmail.com>
2024-08-21 19:05:20 +0100

    test: adding tests with vitest

---
+++ src/queries/__tests__/fetchCurrentAuction.spec.ts
@@ -0,0 +1,11 @@
+import { expect, describe, it, expectTypeOf } from "vitest";
+import { rpcFetchCurrentAuctionInfo } from "../fetchCurrentAuction";
+import { AuctionInfo } from "@/types/AuctionInfo";
+
+describe("rpcFetchCurrentAuctionInfo()", async () => {
+  it("should return the current auction info", async () => {
+    const result = await rpcFetchCurrentAuctionInfo();
+    expect(result).toBeDefined();
+    expectTypeOf(result).toMatchTypeOf<AuctionInfo>();
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    env: {
+      ...config({ path: "./.env.test" }).parsed,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
