d59d4b3010e586ded5296c72a960e475caa2c420
Sandoche <sandoche@protonmail.com>
2024-08-23 13:26:22 +0100

    test: adding missing mocks

--- src/queries/__tests__/fetchBiddingHistory.spec.ts
+++ src/queries/__tests__/fetchBiddingHistory.spec.ts
@@ -1,10 +1,27 @@
-import { expect, describe, it } from 'vitest';
+import { expect, describe, it, expectTypeOf, beforeEach, afterEach, vi } from 'vitest';
 import { fetchBiddingHistory } from '../fetchBiddingHistory';
+import { mockBiddingHistoryResponse } from './mockedData';
+import type { BiddingHistory } from '@/types/BiddingHistory';
+
+beforeEach(() => {
+  vi.mock('../rpcFetchBiddingHistory', async (importOriginal) => {
+    const actual = await importOriginal();
+    return {
+      // @ts-ignore
+      ...actual,
+      rpcFetchBiddingHistory: vi.fn(() => mockBiddingHistoryResponse),
+    };
+  });
+});
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
 
 describe('fetchBiddingHistory(round)', async () => {
   it('should return the bidding history of a given round', async () => {
     const result = await fetchBiddingHistory(BigInt(1));
-    console.log(result);
     expect(result).toBeDefined();
+    expectTypeOf(result).toMatchTypeOf<BiddingHistory>();
   });
 });
--- src/queries/__tests__/fetchCurrentAuctionDates.spec.ts
+++ src/queries/__tests__/fetchCurrentAuctionDates.spec.ts
@@ -1,5 +1,22 @@
-import { expect, describe, it, expectTypeOf } from 'vitest';
+import { expect, describe, it, expectTypeOf, beforeEach, afterEach, vi } from 'vitest';
+import { rpcFetchEpochInfo } from '../rpcFetchEpochInfo';
 import { fetchCurrentAuctionDates } from '../fetchCurrentAuctionDates';
+import { epochInfoResponse } from './mockedData';
+
+beforeEach(() => {
+  vi.mock('../rpcFetchEpochInfo', async (importOriginal) => {
+    const actual = await importOriginal();
+    return {
+      // @ts-ignore
+      ...actual,
+      rpcFetchEpochInfo: vi.fn(() => epochInfoResponse),
+    };
+  });
+});
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
 
 describe('fetchCurrentAuctionDates()', async () => {
   it('should return the start and end dates', async () => {
