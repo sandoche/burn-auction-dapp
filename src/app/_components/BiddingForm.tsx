e06a1b939bcb9fe83834eef9467f2b7d87b182b1
Sandoche <sandoche@protonmail.com>
2024-08-29 16:53:34 +0100

    feat: disabled style implemented

--- src/app/_components/BiddingForm.tsx
+++ src/app/_components/BiddingForm.tsx
@@ -32,7 +32,7 @@ export const BiddingForm = () => {
     send({ type: 'SUBMIT' });
   };
 
-  const isSubmitDisabled = !state.can({ type: 'SUBMIT' }) || state.matches('submitting');
+  const isSubmitDisabled = (state.context.bidAmount !== '' && !state.can({ type: 'SUBMIT' })) || state.matches('submitting');
   const errorMessage =
     Number(state.context.bidAmount) < 0
       ? 'Bid amount must be greater than 0'
@@ -61,7 +61,7 @@ export const BiddingForm = () => {
             disabled={state.matches('submitting')}
           />
           <button
-            className="items-center justify-center rounded-full transition-[background-color,outline-color,filter] transition-200 flex gap-x-1 outline outline-offset-2 outline-1 outline-transparent bg-evmos-orange-500 hover:bg-evmos-orange-400 py-[9px] px-5 active:outline-evmos-secondary-dark"
+            className="disabled:text-evmos-gray-light disabled:bg-evmos-gray disabled:border-evmos-gray items-center justify-center rounded-full transition-[background-color,outline-color,filter] transition-200 flex gap-x-1 outline outline-offset-2 outline-1 outline-transparent bg-evmos-orange-500 hover:bg-evmos-orange-400 py-[9px] px-5 active:outline-evmos-secondary-dark"
             disabled={isSubmitDisabled}
           >
             {state.matches('submitting') ? <LoadingSpinner /> : 'Bid'}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,8 @@ const config: Config = {
         'evmos-secondary-dark': '#55397b',
         'evmos-error': '#e05c4e',
         'evmos-success': '#8ec876',
+        'evmos-gray': '#3a271c',
+        'evmos-gray-light': '#665f5b',
       },
       divideWidth: {
         DEFAULT: '1.5px',
