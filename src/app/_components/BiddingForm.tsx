7dc303cc6e13b6545cfb256313e541569e2bcadd
Sandoche <sandoche@protonmail.com>
2024-08-29 10:07:18 +0100

    feat: adding ui for the bidding form

--- src/app/_components/BiddingForm.tsx
+++ src/app/_components/BiddingForm.tsx
@@ -15,9 +15,15 @@ export const BiddingForm = () => {
             Max
           </a>
         </div>
-        <div className="flex">
-          <input type="text" />
-          <button>Bid</button>
+        <div className="flex pt-2">
+          <input
+            type="text"
+            className="mr-2 w-full rounded-lg transition-all duration-200 p-2 bg-transparent border-2 border-[#3b3634] placeholder:text-[#998e8b] focus:ring-inset focus:ring-[#fe9367]"
+            placeholder="Amount"
+          />
+          <button className="items-center justify-center rounded-full transition-[background-color,outline-color,filter] transition-200 flex gap-x-1 outline outline-offset-2 outline-1 outline-transparent bg-evmos-orange-500 hover:bg-evmos-orange-400 py-[9px] px-5 active:outline-evmos-secondary-dark">
+            Bid
+          </button>
         </div>
       </form>
     </Card>
