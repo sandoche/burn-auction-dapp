1fdc850ea624b8b767fe04f5788b77fcb6c6dce9
Sandoche <sandoche@protonmail.com>
2024-09-06 16:44:49 +0100

    feat: pagination fixed

--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -12,15 +12,22 @@ type TabLink = { name: string; href: string; icon: React.ReactNode };
 export const Tabs = ({ tabs }: Readonly<{ tabs: TabLink[] }>) => {
   const pathname = usePathname();
 
+  const isActive = (tabHref: string) => {
+    if (tabHref === '/') {
+      return pathname === '/';
+    }
+    return pathname.startsWith(tabHref);
+  };
+
   return (
-    <TabGroup selectedIndex={tabs.findIndex((tab) => tab.href === pathname)} className="mb-6">
+    <TabGroup selectedIndex={tabs.findIndex((tab) => isActive(tab.href))} className="mb-6">
       <TabList className="flex bg-evmos-dark rounded-full p-2">
         {tabs.map((tab) => (
           <Tab
             key={tab.name}
             as={Link}
             href={tab.href}
-            className={clsx('py-3 px-4 flex-1 rounded-full flex items-center justify-center text-evmos-light', tab.href === pathname && 'bg-evmos-darkish')}
+            className={clsx('py-3 px-4 flex-1 rounded-full flex items-center justify-center text-evmos-light', isActive(tab.href) && 'bg-evmos-darkish')}
           >
             <div className="mr-1">{tab.icon} </div>
             {tab.name}
