b1f1b99e307b4d8bff34fabe282ca2d80644c34a
Sandoche <sandoche@protonmail.com>
2024-09-06 11:56:55 +0100

    refactor: use of try catch

--- src/queries/prismaFetchAuctionEvent.ts
+++ src/queries/prismaFetchAuctionEvent.ts
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/burn-auction-dapp/blob/main/LICENSE)
 
 import { prisma } from '@/utilities/prisma';
+import { E } from '@/utilities/error-handling';
 
 export const prismaFetchAuctionEvent = async (round: bigint | null = null) => {
-  try {
-    const auctionEndEvents = await prisma.auctionEndEvent.findMany({
+  const [error, auctionEndEvents] = await E.try(() =>
+    prisma.auctionEndEvent.findMany({
       orderBy: {
         blockNumber: 'asc',
       },
@@ -15,11 +16,13 @@ export const prismaFetchAuctionEvent = async (round: bigint | null = null) => {
       include: {
         coins: true,
       },
-    });
+    }),
+  );
 
-    return auctionEndEvents;
-  } catch (error) {
+  if (error) {
     console.error('Error fetching auction end events:', error);
     throw error;
   }
+
+  return auctionEndEvents;
 };
--- src/queries/prismaFetchBidEvent.ts
+++ src/queries/prismaFetchBidEvent.ts
@@ -2,21 +2,24 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/burn-auction-dapp/blob/main/LICENSE)
 
 import { prisma } from '@/utilities/prisma';
+import { E } from '@/utilities/error-handling';
 
 export const prismaFetchBidEvent = async (round: bigint) => {
-  try {
-    const bidEvents = await prisma.bidEvent.findMany({
+  const [error, bidEvents] = await E.try(() =>
+    prisma.bidEvent.findMany({
       orderBy: {
         blockNumber: 'asc',
       },
       where: {
         round: round.toString(),
       },
-    });
+    }),
+  );
 
-    return bidEvents;
-  } catch (error) {
+  if (error) {
     console.error('Error fetching bid events:', error);
     throw error;
   }
+
+  return bidEvents;
 };
