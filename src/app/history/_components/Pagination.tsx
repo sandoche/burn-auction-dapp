8b3b779d8126e0f11a3cb425d958a61f212e15e7
Sandoche <sandoche@protonmail.com>
2024-09-06 15:20:56 +0100

    feat: pagination working

--- src/app/history/_components/Pagination.tsx
+++ src/app/history/_components/Pagination.tsx
@@ -1,45 +1,36 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/burn-auction-dapp/blob/main/LICENSE)
 
-'use client';
-import React from 'react';
+import { ButtonLink } from '@/components/ui/ButtonLink';
+import Image from 'next/image';
+import { clsx } from 'clsx';
 
 interface PaginationProps {
   currentPage: number;
   itemsPerPage: number;
   totalItems: number;
-  // onPageChange: (page: number) => void;
 }
 
 const Pagination: React.FC<PaginationProps> = ({ currentPage, itemsPerPage, totalItems }) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-  const handlePageChange = (page: number) => {
-    if (page < 1 || page > totalPages) return;
-    // onPageChange(page);
-  };
-
   return (
-    <div className="flex justify-center mt-4">
-      <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className="px-4 py-2 mx-1 bg-gray-300 text-gray-700 rounded disabled:opacity-50">
-        Previous
-      </button>
+    <div className="flex justify-center mt-16">
+      {currentPage > 1 && (
+        <ButtonLink href={`/history/${Number(currentPage) - 1}`} className="bg-transparent">
+          <Image src="/icons/arrow-left.svg" alt="Previous" width={20} height={20} />
+        </ButtonLink>
+      )}
       {Array.from({ length: totalPages }, (_, index) => (
-        <button
-          key={index + 1}
-          onClick={() => handlePageChange(index + 1)}
-          className={`px-4 py-2 mx-1 ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} rounded`}
-        >
+        <ButtonLink key={index + 1} href={`/history/${index + 1}`} className={clsx(currentPage != index + 1 && '!bg-transparent')}>
           {index + 1}
-        </button>
+        </ButtonLink>
       ))}
-      <button
-        onClick={() => handlePageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-        className="px-4 py-2 mx-1 bg-gray-300 text-gray-700 rounded disabled:opacity-50"
-      >
-        Next
-      </button>
+      {currentPage < totalPages && (
+        <ButtonLink href={`/history/${Number(currentPage) + 1}`} className="bg-transparent">
+          <Image src="/icons/arrow-right.svg" alt="Next" width={20} height={20} />
+        </ButtonLink>
+      )}
     </div>
   );
 };
